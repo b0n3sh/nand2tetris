4733345b4031c00420d8b751a1bf4237348dc846
b0n3sh <b0n3sh@gmail.com>
2023-10-02 10:58:06 +0200

    Finished Multiplication in assembly (mult.asm) code (4th project)

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,51 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// Pseudocode
+// i=0;
+// res=0;
+// (MULT)
+//     if(i=R1) goto SAVE;
+//     res=res+R0;
+//     i++;
+//     JMP MULT;
+// (SAVE)
+//     R2=res
+// (END)
+//     JMP END;
+
+    // i=0.
+    @i
+    M=0
+    // res=0.
+    @res
+    M=0
+(MULT)
+    // if(i=R1) goto SAVE.
+    @R1
+    D=M
+    @i
+    D=D-M
+    @SAVE
+    D;JEQ
+    // res=res+R0
+    @R0
+    D=M
+    @res
+    M=M+D
+    // i++
+    @i
+    M=M+1
+    // JMP MULT
+    @MULT
+    0;JMP
+(SAVE)    
+    // R2=res
+    @res
+    D=M
+    @R2
+    M=D
+(END)
+    @END
+    0;JMP
