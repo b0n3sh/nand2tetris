42e8ee9ef9bcf1e73d39863aa2d391e9fe077bc0
b0n3sh <b0n3sh@gmail.com>
2024-01-03 22:21:04 +0100

    [WIP] working on the variable creation (other symbols seem fine already)

--- 06/assembler.py
+++ 06/assembler.py
@@ -17,14 +17,25 @@ line_number = 0
 while parser_file.hasMoreLines():
     next_instruction = parser_file.advance()
     print(f"{next_instruction} => {line_number}")
+    if next_instruction[0] == "(":
+        code.sym_map[next_instruction[1:-1]] = line_number+1
     line_number +=1
     
-
 # Second pass 
 parser_file = parser.Parser(file_path)
 while parser_file.hasMoreLines():
     next_instruction = parser_file.advance()
     if parser_file.instructionType() == "C_INSTRUCTION":
-        print(f"111{code.comp(parser_file.comp())}{code.dest(parser_file.dest())}{code.jump(parser_file.jump())}")
+#        print(f"111{code.comp(parser_file.comp())}{code.dest(parser_file.dest())}{code.jump(parser_file.jump())}")
+        pass
     elif parser_file.instructionType() == "A_INSTRUCTION":
-        print(f"0{int(parser_file.symbol()):0>15b}") 
+        # constant number 
+        if parser_file.symbol().isnumeric():
+            print(f"0{int(parser_file.symbol()):0>15b}") 
+        # labels or predefined symbols
+        elif parser_file.symbol() in code.sym_map:
+             print(f"0{int(code.sym_map[parser_file.symbol()]):0>15b}")
+        # variables
+        else:
+            pass
+        
