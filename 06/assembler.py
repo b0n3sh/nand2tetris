c1b1b2f6068f8ba7d167df3d5606a6d1815da522
b0n3sh <b0n3sh@gmail.com>
2024-01-05 12:47:56 +0100

    [WIP] prolly finished the assembler, need to do the tests and then just watch the udemy videos and then can move on 7 project

--- 06/assembler.py
+++ 06/assembler.py
@@ -4,10 +4,21 @@ import parser
 import code
 import sys
 
-if len(sys.argv) != 1:
-    print("usage: ./assembler.py <assembly_file>")
+# Creates a varaible 
+def create_variable(symbol, variable_index):
+    code.sym_map[symbol] = variable_index
 
-file_path = "rect/Rect.asm"
+if len(sys.argv) != 2 or ".asm" not in sys.argv[1]:
+    print("usage: ./assembler.py <assembly_file>")
+    exit()
+
+file_path = sys.argv[1] 
+print(file_path)
+
+print("""[+] Doing first pass... [+]
+   - Keep track of line number
+   - If label declaration is found (xxx) it keeps track of the line number, for posterior jumps 
+""")
 
 # [+] First pass [+]
 #   - Keep track of line number
@@ -16,26 +27,36 @@ parser_file = parser.Parser(file_path)
 line_number = 0
 while parser_file.hasMoreLines():
     next_instruction = parser_file.advance()
-    print(f"{next_instruction} => {line_number}")
     if next_instruction[0] == "(":
-        code.sym_map[next_instruction[1:-1]] = line_number+1
-    line_number +=1
-    
+        code.sym_map[next_instruction[1:-1]] = line_number
+    else:
+        line_number +=1
+
+print("""[+] Doing second pass... [+]
+    - Generating the machine code
+""")
+
+
 # Second pass 
-parser_file = parser.Parser(file_path)
-while parser_file.hasMoreLines():
-    next_instruction = parser_file.advance()
-    if parser_file.instructionType() == "C_INSTRUCTION":
-#        print(f"111{code.comp(parser_file.comp())}{code.dest(parser_file.dest())}{code.jump(parser_file.jump())}")
-        pass
-    elif parser_file.instructionType() == "A_INSTRUCTION":
-        # constant number 
-        if parser_file.symbol().isnumeric():
-            print(f"0{int(parser_file.symbol()):0>15b}") 
-        # labels or predefined symbols
-        elif parser_file.symbol() in code.sym_map:
-             print(f"0{int(code.sym_map[parser_file.symbol()]):0>15b}")
-        # variables
-        else:
-            pass
-        
+
+with open(sys.argv[1].split(".asm")[0]+".hack", "w") as c:
+    parser_file = parser.Parser(file_path)
+    variable_index = 16
+    while parser_file.hasMoreLines():
+        next_instruction = parser_file.advance()
+        if parser_file.instructionType() == "C_INSTRUCTION":
+            print(f"111{code.comp(parser_file.comp())}{code.dest(parser_file.dest())}{code.jump(parser_file.jump())}", file=c)
+        elif parser_file.instructionType() == "A_INSTRUCTION":
+            # constant number 
+            if parser_file.symbol().isnumeric():
+                print(f"0{int(parser_file.symbol()):0>15b}", file=c) 
+            # labels or predefined symbols
+            elif parser_file.symbol() in code.sym_map:
+                print(f"0{int(code.sym_map[parser_file.symbol()]):0>15b}", file=c)
+            # variables
+            else:
+                create_variable(parser_file.symbol(), variable_index)
+                print(f"0{int(code.sym_map[parser_file.symbol()]):0>15b}", file=c)
+                variable_index += 1
+
+print("Done, file created in the same folder with the same name but with .hack extension")
