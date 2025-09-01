6c1c6429711373e42cb7d5dfb805303ae4f5181e
b0n3sh <b0n3sh@gmail.com>
2025-09-01 16:13:13 +0200

    creating generator

--- 10/JackAnalyzer/JackTokenizer.py
+++ 10/JackAnalyzer/JackTokenizer.py
@@ -1,13 +1,20 @@
 class JackTokenizer:
     def __init__(self, file):
-        self.opened_file = self.open_file(file)
+        self.__opened_file = self.open_file(file)
+        self.__next_value = None
+        self.__has_more_tokens = None
+
+    def __next__(self):
+        pass 
     
     def hasMoreTokens(self):
-        next_token = self.opened_file.seek(1)
-        print(next_token)
+        next_token = self.__opened_file
 
     def open_file(self, file):
         with open(file, "r") as f:
-            yield f
+            for line in f:
+                for word in line:
+                    yield(word)
 
 tokenizer = JackTokenizer("test.jack")
+tokenizer.hasMoreTokens()
