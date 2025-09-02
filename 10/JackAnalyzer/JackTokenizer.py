f4a06a026926ce5788df2bd4bbc9f35430a07282
b0n3sh <b0n3sh@gmail.com>
2025-09-02 12:35:57 +0200

    [WIP] creating testing phase for the tokenizer

--- 10/JackAnalyzer/JackTokenizer.py
+++ 10/JackAnalyzer/JackTokenizer.py
@@ -1,14 +1,23 @@
 class JackTokenizer:
     def __init__(self, file):
         self.__opened_file = self.open_file(file)
-        self.__next_value = None
+        self.__next_token = None
         self.__has_more_tokens = None
+        self.__current_token = '' # Will held the full token for the current op
 
     def __next__(self):
-        pass 
+        if self.__has_more_tokens:
+            return self.__next_token
+        else:
+            return next(self.__opened_file)
     
     def hasMoreTokens(self):
-        next_token = self.__opened_file
+        try:
+            self.__next_token = next(self.__opened_file)
+            self.__has_more_tokens = True
+        except:
+            self.__has_more_tokens = False
+        return self.__has_more_tokens
 
     def open_file(self, file):
         with open(file, "r") as f:
@@ -16,5 +25,10 @@ class JackTokenizer:
                 for word in line:
                     yield(word)
 
+    def advance(self):
+        self.__current_token = next(self)
+        return self.__current_token
+        
 tokenizer = JackTokenizer("test.jack")
-tokenizer.hasMoreTokens()
+while tokenizer.hasMoreTokens():
+    print(tokenizer.advance())
