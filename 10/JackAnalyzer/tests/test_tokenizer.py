bfb42ab87315088a85c3fe058ca4ff0bf811ae95
b0n3sh <b0n3sh@gmail.com>
2025-09-05 11:00:15 +0200

    [WIP] creating test for nexttoken

---
+++ 10/JackAnalyzer/tests/test_tokenizer.py
@@ -0,0 +1,21 @@
+import pytest
+
+@pytest.fixture
+def mock_text_file(tmp_path):
+    def _create_file(content):
+        file_path = tmp_path / "mock_file.txt"
+        file_path.write_text(content)
+        return file_path
+    return _create_file 
+
+class TestHasMoreTokens:
+    def test_has_more_tokens(self, mock_text_file):
+        """ Creating mock phrases to check correct has token check """
+        # Arrange
+        file_path = mock_text_file("test")
+        # Act
+        print(file_path)
+        assert False
+        # Assert 
+    def test_doesnt_have_more_tokens(self):
+        pass
